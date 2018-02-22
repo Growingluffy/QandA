1d3a59b62fd9c3deae2d22fb9df2c579ffab2d8d
pl0tz3nh0tz <pl0tz3nh0tz@gmail.com>
2018-02-22 20:32:02 +0100

    register and login post request

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request
 from database import get_db
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -20,13 +22,38 @@ def index():
     return render_template('home.html')
 
 
-@app.route('/register')
+@app.route('/register', methods=['POST', 'GET'])
 def register():
+    if request.method == 'POST':
+
+        db = get_db()
+        hashed_password = generate_password_hash(request.form['password'], method='sha256')
+
+        db.execute('INSERT INTO users (name, password, expert, admin) values (%s, %s, %s, %s)', (request.form['name'], hashed_password, '0', '0', ))
+
+        return '<h1>User created!</h1>'
+
     return render_template('register.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+
+        db = get_db()
+
+        name = request.form['name']
+        password = request.form['password']
+
+        db.execute('SELECT id, name, password from users where name = %s', (name, ))
+        user_result = db.fetchone()
+
+        if check_password_hash(user_result['password'], password):
+            return '<h1>The password is correct!</h1>'
+        else:
+            return '<h1>The password is incorrect!</h1>'
+
+
     return render_template('login.html')
 
 
